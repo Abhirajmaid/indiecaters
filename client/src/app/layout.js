30096a6b869244c760591768260e653383cecf8a
TheAbhiraj <codersground404@gmail.com>
2025-11-18 08:17:06 +0530

    Add Google Analytics tracking script to layout.js

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -71,6 +71,18 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<head>
+				{/* Google tag (gtag.js) */}
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-LZDV7LZZ3C"></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', 'G-LZDV7LZZ3C');
+						`,
+					}}
+				/>
 				<StructuredData />
 			</head>
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
